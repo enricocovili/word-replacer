ba05cc1fad6000d7ec2e3d86a6cadae53dde4257
ginop-1 <enrico.covili@fermi.mo.it>
2021-02-05 19:30:59 +0100

    total rewrite using pathlib

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
-import os
-import re
+from pathlib import Path
 
-def find_sub(pathdir, find_word, replace_word): 
-    for filep in os.scandir(pathdir):
-        if filep.is_dir():
-            if len(os.listdir(filep.path)) == 0: # empty folder
-                continue
-            else:
-                find_sub(filep.path+'/', find_word, replace_word)
-        else:
-    # FOR PERSONAL USE elif os.path.splitext(filep.path)[1] == '.html' or os.path.splitext(filep.path)[1] == '.css' or os.path.splitext(filep.path)[1] == '':
-            with open(filep.path, 'r+') as f:
-                text = f.read()
-                while re.findall(find_word, text): # while find_word is present, continue to modify the file
-                    text = re.sub(find_word, replace_word, text)
-                f.seek(0)
-                f.write(text)
+def main():
+    
+    dir_path = Path(input("path to the folder -> "))
+    target_string = input("insert the word/phrase that will be modified -> ")
+    replacement_string = input("insert the replace word/phrase -> ")
 
-path = input("path to the folder -> ")
-if path[len(path)-1] != '/':
-    path += '/'
-find_word = input("insert the word/phrase that will be modified -> ")
-replace_word = input("insert the replace word/phrase -> ")
-# print(path)
-# filepath = os.scandir(path+'/')
-find_sub(path, find_word, replace_word)
+    if Path(dir_path).is_dir():
+        files_to_edit = dir_path.glob('**/*.*')    
+        for file in files_to_edit:
+            word_replacer(file, target_string, replacement_string)
 
+    elif Path(dir_path).is_file():
+        word_replacer(dir_path, target_string, replacement_string)
 
+    else:
+        print("incorrect path")
+
+def word_replacer(file, target_string, replacement_string):
+    initial_text = file.read_text()
+    replacement_text = initial_text.replace(target_string, replacement_string)
+    file.write_text(replacement_text)
+
+if __name__ == "__main__":
+    main()
